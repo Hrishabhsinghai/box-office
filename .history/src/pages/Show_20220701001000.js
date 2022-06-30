7563b13b31ddc097e95d041f615be3e6d9b02d7d
Hrishabh Jain <hrishabhsinghai77@gmail.com>
2022-07-01 00:28:21 +0530

    added show page

---
+++ .history/src/pages/Show_20220701001000.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const Show = () => {
+  const params = useParams();
+  console.log('params', params);
+
+  return <div>this is show page</div>;
+};
+
+export default Show;
